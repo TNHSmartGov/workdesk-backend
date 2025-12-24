f34b38a200786594bc538815c2cae68e621d5af4
tiengthettheky <32325354+tiengthettheky@users.noreply.github.com>
2025-12-24 11:25:26 +0700

    sss

--- src/main/java/com/tnh/baseware/core/dtos/project/ProjectDTO.java
+++ src/main/java/com/tnh/baseware/core/dtos/project/ProjectDTO.java
@@ -1,5 +1,6 @@
 package com.tnh.baseware.core.dtos.project;
 
+import com.tnh.baseware.core.dtos.basic.BasicOrganizationDTO;
 import com.tnh.baseware.core.entities.audit.Identifiable;
 import com.tnh.baseware.core.enums.project.ProjectStatus;
 import lombok.*;
--- src/main/java/com/tnh/baseware/core/forms/project/ProjectEditorForm.java
+++ src/main/java/com/tnh/baseware/core/forms/project/ProjectEditorForm.java
@@ -2,8 +2,6 @@ package com.tnh.baseware.core.forms.project;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import com.tnh.baseware.core.enums.project.ProjectStatus;
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -20,9 +18,6 @@ import java.util.UUID;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ProjectEditorForm {
-
-    @NotBlank(message = "{code.not.blank}")
-    private String code;
 
     @NotBlank(message = "{name.not.blank}")
     String name;

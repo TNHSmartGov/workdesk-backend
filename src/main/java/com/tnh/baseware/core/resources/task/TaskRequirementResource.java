10ff78a8b65c8742b55a14b8598f58c0a0e6da66
NgaNT01 <tanngatn01@gmail.com>
2026-01-07 16:56:58 +0700

    add task requirement apis

--- src/main/java/com/tnh/baseware/core/resources/task/TaskRequirementResource.java
+++ src/main/java/com/tnh/baseware/core/resources/task/TaskRequirementResource.java
@@ -2,18 +2,21 @@ package com.tnh.baseware.core.resources.task;
 
 import com.tnh.baseware.core.dtos.task.TaskRequirementDTO;
 import com.tnh.baseware.core.entities.task.TaskRequirement;
+import com.tnh.baseware.core.forms.task.AssignRequirementForm;
 import com.tnh.baseware.core.forms.task.TaskRequirementEditorForm;
 import com.tnh.baseware.core.properties.SystemProperties;
 import com.tnh.baseware.core.resources.GenericResource;
 import com.tnh.baseware.core.services.IGenericService;
 import com.tnh.baseware.core.services.MessageService;
 import com.tnh.baseware.core.services.task.ITaskRequirementService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @Tag(name = "Task Requirements", description = "API for managing task requirements")
@@ -30,5 +33,46 @@ public class TaskRequirementResource extends GenericResource<TaskRequirement, Ta
                                    ITaskRequirementService taskRequirementService) {
         super(service, messageService, systemProperties.getApiPrefix() + "/task-requirements");
         this.taskRequirementService = taskRequirementService;
+    }
+
+    @Operation(summary = "Add requirement")
+    @PostMapping("/{taskId}/requirements")
+    public TaskRequirementDTO addRequirement(@PathVariable UUID taskId,
+                                             @RequestBody @Valid TaskRequirementEditorForm form) {
+        return taskRequirementService.create(taskId, form);
+    }
+
+    @Operation(summary = "Update requirement")
+    @PutMapping("/{taskId}/requirements/{requirementId}")
+    public TaskRequirementDTO updateRequirement(@PathVariable UUID taskId,
+                                                @PathVariable UUID requirementId,
+                                                @RequestBody @Valid TaskRequirementEditorForm form) {
+        return taskRequirementService.update(taskId, requirementId, form);
+    }
+
+    @Operation(summary = "Delete requirement")
+    @DeleteMapping("/{taskId}/requirements/{requirementId}")
+    public void deleteRequirement(@PathVariable UUID taskId, @PathVariable UUID requirementId) {
+        taskRequirementService.delete(taskId, requirementId);
+    }
+
+    @Operation(summary = "Assign requirement to member")
+    @PatchMapping("/{taskId}/requirements/{requirementId}/assign")
+    public TaskRequirementDTO assignRequirement(@PathVariable UUID taskId,
+                                                @PathVariable UUID requirementId,
+                                                @RequestBody @Valid AssignRequirementForm form) {
+        return taskRequirementService.assignToMember(taskId, requirementId, form);
+    }
+
+    @Operation(summary = "Toggle requirement complete")
+    @PatchMapping("/{taskId}/requirements/{requirementId}/toggle")
+    public void toggleRequirement(@PathVariable UUID taskId, @PathVariable UUID requirementId) {
+        taskRequirementService.toggleComplete(taskId, requirementId);
+    }
+
+    @Operation(summary = "Get task requirements")
+    @GetMapping("/{taskId}/requirements")
+    public List<TaskRequirementDTO> getRequirements(@PathVariable UUID taskId) {
+        return taskRequirementService.getByTaskId(taskId);
     }
 }

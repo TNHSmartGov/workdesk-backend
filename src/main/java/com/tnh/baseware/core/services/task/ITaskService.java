06cfca33cbd81c0b1a53905c2ec938a01fc78b1d
vuong <vuongit.design@gmail.com>
2026-01-10 17:11:58 +0700

    xóa các import không cần thiết

--- src/main/java/com/tnh/baseware/core/services/task/ITaskService.java
+++ src/main/java/com/tnh/baseware/core/services/task/ITaskService.java
@@ -2,8 +2,6 @@ package com.tnh.baseware.core.services.task;
 
 import com.tnh.baseware.core.dtos.task.TaskDTO;
 import com.tnh.baseware.core.entities.task.Task;
-import com.tnh.baseware.core.enums.task.MemberStatus;
-import com.tnh.baseware.core.enums.task.TaskAction;
 import com.tnh.baseware.core.forms.task.TaskEditorForm;
 import com.tnh.baseware.core.services.IGenericService;
 
